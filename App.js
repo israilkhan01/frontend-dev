b7b6a9a0f41eea436e5df1a7dda83ed91cbca6eb
israilkhan01 <israil.khan.a888@gmail.com>
2024-01-08 00:04:08 +0530

    React compositions

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 const heading  = React.createElement("h1",{
     id:"heading"
 },"Hello world from React");
@@ -11,10 +11,16 @@ const jsxHeading = <h1>Welcome to JSX inside React </h1>;
 
 
 //React Component
+//React composition
 const HeadingComponent = ()=>{
-    return <h1>Welcome to React Component</h1>
+    return (
+        <div className="container">
+            {jsxHeading}
+            <h1>Welcome to React Component</h1>
+        </div>
+    )
 }
 console.log(jsxHeading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent)
+console.log(HeadingComponent)
 root.render(<HeadingComponent/>)
